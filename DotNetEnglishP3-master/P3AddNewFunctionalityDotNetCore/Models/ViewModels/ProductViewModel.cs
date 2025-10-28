0ed0e209c9e82f4eaa151033557e78608efd8049
Pierre LaMogne <pierrelamogne@gmail.com>
2025-10-28 14:49:52 +0100

    Get ProductViewModel verification by DataAnnotations

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Resources.Models.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +10,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "MissingStock")]
+        [RegularExpression(@"^\s*-?\d+\s*$", ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "StockNotAnInteger")]
+        [Range(1, int.MaxValue, ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "StockNotGreaterThanZero")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "MissingPrice")]
+        [RegularExpression(@"^\s*-?\d+([.,]\d+)?\s*$", ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "PriceNotANumber")]
+        [Range(0.01, double.MaxValue, ErrorMessageResourceType = typeof(ProductService), ErrorMessageResourceName = "PriceNotGreaterThanZero")]
         public string Price { get; set; }
     }
 }
